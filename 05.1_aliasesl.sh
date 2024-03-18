62adb94bd135f00fbd108be1a4a5cf427293f5db
Epvre <43313986+Epvre@users.noreply.github.com>
2024-03-18 19:26:07 +0300

    Update 05.1_aliasesl.sh

--- 05.1_aliasesl.sh
+++ 05.1_aliasesl.sh
@@ -3,9 +3,28 @@ A_CF=~/.bashrc
 > $A_CF
 
 echo "
+[[ $- != *i* ]] && return
+
+[[ $DISPLAY ]] && shopt -s checkwinsize
+
 alias ls='ls -lah --color=auto'
 alias grep='grep --color=auto'
 PS1='\u@\h \W]\$ '
+
+case ${TERM} in
+  Eterm*|kitty*|alacritty*|aterm*|foot*|gnome*|konsole*|kterm*|putty*|rxvt*|tmux*|x>
+    PROMPT_COMMAND+=('printf "\033]0;%s@%s:%s\007" "${USER}" "${HOSTNAME%%.*}" "${P>
+
+    ;;
+  screen*)
+    PROMPT_COMMAND+=('printf "\033_%s@%s:%s\033\\" "${USER}" "${HOSTNAME%%.*}" "${P>
+    ;;
+esac
+
+if [[ -r /usr/share/bash-completion/bash_completion ]]; then
+  . /usr/share/bash-completion/bash_completion
+fi
+
 
 eval "$(starship init bash)"
 EDITOR=nano
