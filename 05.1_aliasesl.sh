e9d9a401a7e554c8885822c4581383202bbd00e0
Epvre <43313986+Epvre@users.noreply.github.com>
2024-03-18 19:59:25 +0300

    Update 05.1_aliasesl.sh

--- 05.1_aliasesl.sh
+++ 05.1_aliasesl.sh
@@ -4,35 +4,33 @@ pacman -S --noconfirm --needed nano-syntax-highlighting
 
 BASHRC=/etc/bash.bashrc
 > $BASHRC
-
-echo "
-[[ $- != *i* ]] && return
-
-[[ $DISPLAY ]] && shopt -s checkwinsize
-
-alias ls='ls -lah --color=auto'
-alias grep='grep --color=auto'
-PS1='\u@\h \W]\$ '
-
-case ${TERM} in
-  Eterm*|kitty*|alacritty*|aterm*|foot*|gnome*|konsole*|kterm*|putty*|rxvt*|tmux*|x>
-    PROMPT_COMMAND+=('printf "\033]0;%s@%s:%s\007" "${USER}" "${HOSTNAME%%.*}" "${P>
-
-    ;;
-  screen*)
-    PROMPT_COMMAND+=('printf "\033_%s@%s:%s\033\\" "${USER}" "${HOSTNAME%%.*}" "${P>
-    ;;
-esac
-
-if [[ -r /usr/share/bash-completion/bash_completion ]]; then
-  . /usr/share/bash-completion/bash_completion
-fi
-
-
-eval "$(starship init bash)"
-export EDITOR=nano
-" >> $BASHRC
-chmod +x BASHRC
+  echo "
+  [[ $- != *i* ]] && return
+  
+  [[ $DISPLAY ]] && shopt -s checkwinsize
+  
+  alias ls='ls -lah --color=auto'
+  alias grep='grep --color=auto'
+  PS1='\u@\h \W]\$ '
+  
+  case ${TERM} in
+    Eterm*|kitty*|alacritty*|aterm*|foot*|gnome*|konsole*|kterm*|putty*|rxvt*|tmux*|x>
+      PROMPT_COMMAND+=('printf "\033]0;%s@%s:%s\007" "${USER}" "${HOSTNAME%%.*}" "${P>
+  
+      ;;
+    screen*)
+      PROMPT_COMMAND+=('printf "\033_%s@%s:%s\033\\" "${USER}" "${HOSTNAME%%.*}" "${P>
+      ;;
+  esac
+  
+  if [[ -r /usr/share/bash-completion/bash_completion ]]; then
+    . /usr/share/bash-completion/bash_completion
+  fi
+  
+  eval "$(starship init bash)"
+  export EDITOR=nano
+  " >> $BASHRC
+  chmod +x BASHRC
 
  # Get a list of all users in /home
 USERS=$(ls /home)
